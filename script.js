8161cce0ee8151e36c9a67026d60988fecf91650
Glerts <kocanmeric@gmail.com>
2021-12-10 23:43:58 +0300

    Add score board and some CSS

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 var playerScore = 0;
 var computerScore = 0;
 var playerSelection;
+const buttons = document.getElementsByClassName("buttons");
+const newGame = document.getElementById("newgame");
+newGame.disabled = true;
+
+function disable(){
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = true;
+    }
+    newGame.disabled = false;
+}
+
+function winner(playerScore, computerScore){
+
+    //To chose for winner
+
+    if (playerScore == 5){
+        document.getElementById("score").innerHTML = "You Win!";
+        disable();
+       }else if(computerScore == 5){
+        document.getElementById("score").innerHTML = "Computer Win!";
+        disable();
+    }else{
+        return
+    }
+}
 
 function computerPlay(){
+
+    //For computer chose randomly
+
     const wepons = ["rock","paper","scissors"];
     return wepons[Math.floor(Math.random() * wepons.length)];
 }
 
 function playRound(playerSelection, computerSelection){
-    
+
+    //Game mechanics
     
     if(playerSelection == "rock" && computerSelection == "paper"){
         computerScore += 1;
@@ -36,24 +65,44 @@ function playRound(playerSelection, computerSelection){
         return "It is draw!";
     }
 
+//Buttons coding
 
 document.getElementById("rock").addEventListener("click", function(){
     playerSelection = "rock";
     computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection))});
+    console.log(playRound(playerSelection,computerSelection));
+    document.getElementById("player").innerHTML = playerScore;
+    document.getElementById("computer").innerHTML = computerScore;
+    winner(playerScore,computerScore);
+});
+    
 
 document.getElementById("paper").addEventListener("click", function(){
     playerSelection = "paper";
     computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection))});
+    console.log(playRound(playerSelection,computerSelection))
+    document.getElementById("player").innerHTML = playerScore;
+    document.getElementById("computer").innerHTML = computerScore;
+    winner(playerScore,computerScore);
+});
 
 document.getElementById("scissor").addEventListener("click", function(){
     playerSelection = "scissors";
     computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection))});
-        
+    console.log(playRound(playerSelection,computerSelection));
+    document.getElementById("player").innerHTML = playerScore;
+    document.getElementById("computer").innerHTML = computerScore;
+    winner(playerScore,computerScore);
+});
 
-if(computerScore == 5){
-    console.log("Computer Win!") ;
-}else if(playerSelection == 5){
-    console.log("You Win!");}
+document.getElementById("newgame").addEventListener("click", function(){
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].disabled = false;
+    }
+    document.getElementById("player").innerHTML = playerScore = 0;
+    document.getElementById("computer").innerHTML = computerScore = 0;
+    document.getElementById("score").innerHTML = "";
+    newGame.disabled = true;
+    console.clear();
+})
+        
